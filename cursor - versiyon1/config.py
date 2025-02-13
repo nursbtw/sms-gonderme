e725a62d0b270fdcc556fff4e8ac631a18be1b17
nur sena <94234296+nursbtw@users.noreply.github.com>
2025-02-13 16:40:57 +0300

    Delete cursor - versiyon1 directory

--- cursor - versiyon1/config.py
+++
@@ -1,6 +0,0 @@
-SMS_API_CONFIG = {
-    'API_URL': 'https://yurticisms1.com/sms_api',
-    'USERNAME': 'user',
-    'PASSWORD': 'Tr**ys^4e',
-    'DATACODING': 'turkish'  # default, unicode, or turkish
-} 
